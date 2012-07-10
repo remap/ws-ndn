678e9b36e070614be4355845b2ebe1df7803c999
Anmol <anmol.rajpurohit@gmail.com>
2012-07-10 06:43:38 -0700

    working websocket server

--- README.md
+++ README.md
@@ -11,11 +11,21 @@ To compile ws_server.c link to crypto library as follow :
 
 gcc ws_server.c -lcrypto -o ws_server
 
+To see websocket server functioning :
 
+-> Start C server 
+-> Open /web/webscoket-client.html in Mozilla Firefox
+-> Open JavaScript Console
 
-notes on websockets
-===================
+	For Firefox:
 
+	    Press CTRL + SHIFT + K to open the Web console.
+				OR
+	    If Firebug is installed: Press F12 to open Firebug and click on the "Console" tab
+
+Implementation
+==============
+ 
 websocket implementation varies across browsers (http://caniuse.com/#feat=websockets); primarily the handshakes are different (http://goo.gl/Osuxk). It's difficult to say which implementation will finally be widely supported, so we are using what is easiest for us now to develop the server (RFC 6455 ?). 
 
 The server side socket implementation for CCNx will be done in a manner that allows easy change of handshake protocol, but the data and encoding should remain constant.
@@ -23,3 +33,53 @@ The server side socket implementation for CCNx will be done in a manner that all
 lwNDN - the intended final client - sidestepped this issue by using java for a TCP socket, instead of wrestling with websockets just yet.
 
 For testing, we used [insert browser] with the websocket-client.html in /web
+
+Notes on websockets
+===================
+
+WebSocket is a web technology providing for bi-directional, full-duplex communications channels over a single TCP connection. The WebSocket API is being standardized by the W3C, and the WebSocket protocol has been standardized by the IETF as RFC 6455.
+
+To establish a WebSocket connection, the client sends a WebSocket handshake request, and the server sends a WebSocket handshake response, as shown in the following example:
+
+Client request :
+
+GET / HTTP/1.1
+Host: server.example.com
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:13.0) Gecko/20100101 Firefox/13.0.1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-us,en;q=0.5
+Accept-Encoding: gzip, deflate
+Connection: keep-alive, Upgrade
+Sec-WebSocket-Version: 13
+Origin: null
+Sec-WebSocket-Key: fl/GIXZGTVxEOpjTLObP4w==
+X-DNT-OVersion: 2.2.0.515
+X-DNT-Cohort: 5
+X-DNT-Version: 2.2.1.611 FF ffamo 6659
+Pragma: no-cache
+Cache-Control: no-cache
+Upgrade: websocket
+
+Server response:
+
+HTTP/1.1 101 Web Socket Protocol Handshake
+Upgrade: Websocket
+Connection: Upgrade
+Sec-WebSocket-Accept: HSmrc0sMlYUkAGmm5OPpG2HaGWk=
+WebSocket-Origin: http://example.com
+WebSocket-Location: ws://example.com/bin/demo
+
+Note that each line ends with an EOL (end of line) sequence, \n or \r\n. There must be a blank line at the end.
+The client sends a Sec-WebSocket-Key which is base64 encoded. To form a response, the magic string 258EAFA5-E914-47DA-95CA-C5AB0DC85B11 is appended to this (undecoded) key. The resulting string is then hashed with SHA-1, then base64 encoded. Finally, the resulting reply occurs in the header Sec-WebSocket-Accept.
+Details of Sec-WebSocket-Key to Sec-WebSocket-Accept :
+x3JJHMbDL1EzLkh9GBhXDw==258EAFA5-E914-47DA-95CA-C5AB0DC85B11 string hashed by SHA-1 gives 1d29ab734b0c9585240069a6e4e3e91b61da1969 hexadecimal value.
+Encoding the SHA-1 hash by Base64 yields HSmrc0sMlYUkAGmm5OPpG2HaGWk=, which is the Sec-WebSocket-Accept value.
+Once the connection is established, the client and server can send WebSocket data frames back and forth in full-duplex mode. They can send text frames in full-duplex, in either direction at the same time. The data is minimally framed.
+
+In the WebSocket Protocol, data is transmitted using a sequence of frames.
+The server MUST close the connection upon receiving a frame that is not masked. In this case, a server MAY send a Close frame with a status code of 1002 (protocol error). A server MUST NOT mask any frames that it sends to the client. A client MUST close a connection if it detects a masked frame.  In this case, it MAY use the status code 1002(protocol error)
+
+For more information regarding data framing, masking/unmasking fo through RFC 6455. 
+Link : http://tools.ietf.org/html/rfc6455
+
+
--- ws_server.c
+++ ws_server.c
@@ -11,114 +11,158 @@
 #include <openssl/evp.h>
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
+#include <assert.h>
 
 /* Function to convert hash into base 64*/
 char *base64(const unsigned char *input, int length);
 
 int main( int argc, char *argv[] )
 {
-    int sockfd, newsockfd, portno, clilen,len,n;
-    char buffer[256], buf[26];
-    struct sockaddr_in serv_addr, cli_addr;
-    char handshake[360];
-    char message[360];
-    char *handshake_part2, *handshake_part3,*key,*magic,*final;
-    const void *hand;
-    unsigned char hash [20];
-    int handshaken = 0;
+	/* For creating, handling and passing data across socket */
+	int sockfd, newsockfd, portno, clilen,len,n;
+	char buffer[656];
+	struct sockaddr_in serv_addr, cli_addr;
     
-    /* First call to socket() function */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-    {
-        perror("ERROR opening socket");
-        exit(1);
-    }
+	/* For http handshake to initiate web socket protocol */
+	char handshake[360],message[360];
+	char *handshake_part2, *handshake_part3,*key,*magic,*final;
+	const void *hand;
+    	unsigned char hash [20];
+        int handshaken = 0;
     
-    /* Initialize socket structure */
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = 5001;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+    	/* For masking and unmasking data sent or recieved*/
+    	char data[500];
+	char *in, *buffr;
+	unsigned int i;
+	unsigned char mask[4];
+	unsigned int packet_length = 0;
+	int rc;
+	char frame[131];
  
-    /* Now bind the host address using bind() call.*/
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                          sizeof(serv_addr)) < 0)
-    {
-         perror("ERROR on binding");
-         exit(1);
-    }
-
-    /* Now start listening for the clients, here process will
-    * go in sleep mode and will wait for the incoming connection
-    */
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-
-    /* Accept actual connection from the client */
-    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, 
-                                &clilen);
-    if (newsockfd < 0) 
-    {
-        perror("ERROR on accept");
-        exit(1);
-    }
-    
-    /* If connection is established then start communicating */
-    
-    while (1)
-
-	if (handshaken == 0)
-	{
-		bzero(buffer,256);
-    		n = read( newsockfd,buffer,255 );
-    		if (n < 0)
-    		{
-        		perror("ERROR reading from socket");
-        		exit(1);
-    		}
-    		printf("Here is the message:\n%s\n",buffer);
-    		
-                /* Preparing http request response to establish socket */
-   		key=strndup(buffer+98, 24);
-   		magic = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-    		final = strcat(key,magic);
-    		SHA1(final,strlen(final),hash);
-    		handshake_part2= base64(hash, sizeof(hash));
-     
-    		strcpy (handshake,"HTTP/1.1 101 Web Socket Protocol Handshake\nUpgrade: Websocket\nConnection: Upgrade\nSec-WebSocket-Accept: ");
-    		strcat(handshake,handshake_part2);
-    		handshake_part3="\nWebSocket-Origin: http://localhost:80\nWebSocket-Location: ws://localhost:80/\r\n\r\n";
-    		strcat(handshake,handshake_part3);
-
-  		/* Write a http response header to the client */
-  	        n = write(newsockfd, handshake, strlen(handshake));
-    		handshaken=1;
-    		if (n < 0)
-    		{
-        	perror("ERROR writing to socket");
+	/* First call to socket() function */
+    	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    	if (sockfd < 0) 
+    	{
+       		perror("ERROR opening socket");
         	exit(1);
-    		}
-	}
-	else
-	{
-		bzero(buffer,256);
-    		n = read( newsockfd,buffer,255 );
-    		if (n < 0)
-    		{
-        		perror("ERROR reading from socket");
+    	}
+    
+    	/* Initializing socket structure */
+    	bzero((char *) &serv_addr, sizeof(serv_addr));
+    	portno = 5001;
+    	serv_addr.sin_family = AF_INET;
+    	serv_addr.sin_addr.s_addr = INADDR_ANY;
+    	serv_addr.sin_port = htons(portno);
+ 
+    	/* Now binding the host address using bind() call.*/
+    	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    	{
+        	perror("ERROR on binding");
+        	exit(1);
+    	}
+
+    	/* Now start listening for the clients, here process will
+    	* go in sleep mode and will wait for the incoming connection
+    	*/
+    	listen(sockfd,5);
+    	clilen = sizeof(cli_addr);
+
+	/* Accept actual connection from the client */
+    	newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    	if (newsockfd < 0) 
+    	{
+        	perror("ERROR on accept");
+        	exit(1);
+    	}
+    
+    	/* If connection is established then start communicating */
+    	while (1)
+    	{
+		if (handshaken == 0)	// Proceed with http handshake if not done
+		{
+			bzero(buffer,656);
+    			n = read( newsockfd,buffer,655 );
+    			if (n < 0)
+    			{
+        			perror("ERROR reading from socket");
+        			exit(1);
+    			}
+    			printf("HTTP Request Header :\n%s\n",buffer);
+    			len= strlen(buffer);
+			//printf("%d",len);			
+        	        /* Preparing http request response to establish socket */
+   			key=strndup(buffer+359, 24);	// Extracting WebSocket-Key 
+        	        magic = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    			final = strcat(key,magic);	//Appending magic key to WebSocket-Key   
+    			SHA1(final,strlen(final),hash);		//String hashing by SHA 1
+    			handshake_part2= base64(hash, sizeof(hash)); // Encoding the SHA-1 hash by Base64
+     			strcpy (handshake,"HTTP/1.1 101 Web Socket Protocol Handshake\r\nUpgrade: Websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ");
+    			strcat(handshake,handshake_part2);
+    			handshake_part3="\r\nWebSocket-Origin: http://localhost:80\r\nWebSocket-Location: ws://localhost:80/\r\n\r\n";
+    			strcat(handshake,handshake_part3);
+	
+  			/* Write a http response header to the client */
+  		        n = write(newsockfd, handshake, strlen(handshake));
+    			handshaken=1;
+    			if (n < 0)
+    			{
+        		perror("ERROR writing to socket");
         		exit(1);
-    		}
-		len=strlen(buffer);
-    		printf("%d",len);
-                printf("Here is the message:\n%s\n",buffer);
-                strcpy(message,"Server here !");
-                n = write(newsockfd, message, strlen(message));
-      	}     
-    return 0; 
+    			}
+		}
+		else
+		{
+			/* Reading content recieved from client */			
+			bzero(data,500);
+    			n = read( newsockfd,data,499);
+    			if (n < 0)
+    			{
+        			perror("ERROR reading from socket");
+        			exit(1);
+    			}
+			
+                	in=data;
+			assert(in[0] == '\x81');
+			packet_length = ((unsigned char) in[1]) & 0x7f;
+                	//printf("\nPacket length : %d\n", packet_length);
+			mask[0] = in[2];
+			mask[1] = in[3];
+			mask[2] = in[4];
+			mask[3] = in[5];
+                	if(packet_length <= 126)
+			{                      
+				/* Unmask the payload. */
+				for (i = 0; i < packet_length; i++)
+				in[6 + i] ^= mask[i % 4];
+			  	rc = asprintf(&buffr, "%.*s", packet_length, in + 6); 
+			}
+			else if(packet_length == 127)
+			{            
+	  			/* Unmask the payload. */
+	  			for (i = 0; i < packet_length; i++)
+	   			 in[8 + i] ^= mask[i % 4];
+	  			rc = asprintf(&buffr, "%.*s", packet_length, in + 8);
+			}
+	
+
+			printf("Here is the message from client :\n%s\n",buffr);
+			strcpy(message,"Hello Client (from server)");                
+			/* Framing data to be sent to client */
+			frame[0] = '\x81';
+			frame[1] = 128 + strlen(message);
+			frame[2] = '\x00';
+			frame[3] = '\x00';
+			frame[4] = '\x00';
+			frame[5] = '\x00';
+			snprintf(frame+6, 124, "%s", message);
+                	n = write(newsockfd, frame, 6+strlen(message));
+			
+      		}	
+      	};     
+    return 0; 	
 }
 
+/* Function to convert hash into base 64*/
 char *base64(const unsigned char *input, int length)
 {
 BIO *bmem, *b64;
